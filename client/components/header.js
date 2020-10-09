9282ced685cad6c459318ed75db5ec775ee39821
Vladimir Valdin <vladimir.valdin@trivago.com>
2020-10-09 23:11:11 +0200

    homework completed v.1.3 added IDs

--- client/components/header.js
+++ client/components/header.js
@@ -5,7 +5,9 @@ const Header = () => {
   const linkToGo = useParams()
   return (
     <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
-      <Link to="/" id="go-back">Go Back</Link>
+      <Link to="/" id="go-back">
+        Go Back
+      </Link>
       <div id="repository-name">{linkToGo.userName}:</div>
     </div>
   )
